316a6a7e5f1f4d80e24dfe518d54763d63da8aeb
wtimme <wtimme@users.noreply.github.com>
2019-04-12 15:45:26 +0200

    #203 Present the "POI Type" view controller when long-tapping on "Add Button"

--- src/iOS/MapViewController.swift
+++ src/iOS/MapViewController.swift
@@ -12,9 +12,19 @@ extension MapViewController {
     @IBAction func didLongTapOnAddButton(_ sender: AnyObject) {
         let storyboard = UIStoryboard(name: "MainStoryboard", bundle: nil)
         
-        let poiTabBarController = storyboard.instantiateViewController(withIdentifier: "poiTabBar")
-        self.present(poiTabBarController, animated: true) {
-            // TODO
+        guard let poiTabBarController = storyboard.instantiateViewController(withIdentifier: "poiTabBar") as? POITabBarController else {
+            return
+        }
+        
+        // Make sure that the "Common Tags" item is selected.
+        poiTabBarController.selectedIndex = 0
+        
+        present(poiTabBarController, animated: true) {
+            guard let navigationController = poiTabBarController.viewControllers?.first as? UINavigationController else {
+                return
+            }
+            
+            navigationController.viewControllers.last?.performSegue(withIdentifier: "POITypeSegue", sender: nil)
         }
     }
 }
