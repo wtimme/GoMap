565119bec87a56f7e12c051bf93755d523fffcaa
wtimme <wtimme@users.noreply.github.com>
2020-09-25 00:55:49 +0200

    Let the action download the address-formats.json file

--- src/iOS/fastlane-plugin-gomap/lib/fastlane/plugin/gomap/actions/update_presets_action.rb
+++ src/iOS/fastlane-plugin-gomap/lib/fastlane/plugin/gomap/actions/update_presets_action.rb
@@ -1,11 +1,34 @@
 require 'fastlane/action'
 require_relative '../helper/gomap_helper'
+require "open-uri"
+require "fileutils"
+require "json"
 
 module Fastlane
   module Actions
     class UpdatePresetsAction < Action
       def self.run(params)
-        UI.message("This is the update_presets action")
+        downloadLatestPresets
+      end
+
+      def self.downloadLatestPresets()
+        tag = "v2.18.5"
+
+        address_formats_url = "https://raw.githubusercontent.com/openstreetmap/iD/#{tag}/dist/data/address_formats.min.json"
+        address_formats_file = "../presets/address-formats.json"
+        download(address_formats_url, address_formats_file)
+      end
+
+      def self.download(url, path)
+        case io = open(url)
+        when StringIO then File.open(path, 'w') { |f|
+          json = JSON.parse(io.string)
+          pretty_json = JSON.pretty_generate(json)
+
+          f.write(pretty_json)
+        }
+        when Tempfile then io.close; FileUtils.mv(io.path, path)
+        end
       end
 
       def self.description
